712d6a3c86d03e168996dac15516990561280750
Iaasaap <i.saulmar@gmail.com>
2023-04-08 17:55:43 -0700

    first commit grid created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const GridButton = ({ row, col, toggleButton, isOn }) => {
+  const handleClick = () => {
+    toggleButton(row, col);
+  };
+
+  return (
+    <button className={`grid-button ${isOn ? 'on' : 'off'}`} onClick={handleClick}>
+      {isOn ? 'On' : 'Off'}
+    </button>
+  );
+};
+
+const App = () => {
+  const rows = 7;
+  const cols = 8;
+  const initialGrid = Array.from({ length: rows }, () => Array(cols).fill(false));
+
+  const [grid, setGrid] = useState(initialGrid);
+
+  const toggleButton = (row, col) => {
+    setGrid(prevGrid => {
+      const newGrid = prevGrid.map((rowArr, i) => rowArr.map((cell, j) => (i === row && j === col) ? !cell : cell));
+      return newGrid;
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>7x8 Grid of Buttons</h1>
+      <div className="grid-container">
+        {grid.map((rowArr, rowIndex) => (
+          <div key={rowIndex} className="grid-row">
+            {rowArr.map((isOn, colIndex) => (
+              <GridButton
+                key={`${rowIndex}-${colIndex}`}
+                row={rowIndex}
+                col={colIndex}
+                toggleButton={toggleButton}
+                isOn={isOn}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
